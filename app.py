47cd7f206cd138c30345d6325fccae7d2620a6d5
skkukretisk <kukretikumar103@gmail.com>
2024-06-05 11:49:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -92,10 +92,14 @@ async def messages(req: Request) -> Response:
     return Response(status=201)
 
 
-APP = web.Application(middlewares=[aiohttp_error_middleware])
-APP.router.add_post("/api/messages", messages)
+def init_func(argv):
+    APP = web.Application(middlewares=[aiohttp_error_middleware])
+    APP.router.add_post("/api/messages", messages)
+    return APP
+
 
 if __name__ == "__main__":
+    APP = init_func(None)
     try:
         web.run_app(APP, host="localhost", port=CONFIG.PORT)
     except Exception as error:
