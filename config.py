3881b2bcc8e458beb54e754cc993483290ca1bba
skkukretisk <kukretikumar103@gmail.com>
2024-06-05 12:40:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "dec529fb-85bb-442c-8f47-e57f6fc50e6a")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "LZQ8Q~K3msg3xWaExWIxiUddAsDK4kRKg_wk1aHV")
     EXPIRE_AFTER_SECONDS = os.environ.get("ExpireAfterSeconds", 300)
